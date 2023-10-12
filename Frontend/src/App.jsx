30a18eb02e8da3712013c3970bbb253400eae0f7
anaxz <annah704@gmail.com>
2023-10-12 16:36:02 +0100

    Go back to old commit. Staging branch to current has issues

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { Routes, Route } from "react-router-dom"
-
-import { Itinerary } from './components'
 
 function App() {
 
   return (
     <>
       <h1>Welcome</h1>
-
-      <Routes>
-        <Route path="/" element={<Itinerary />} ></Route>
-      </Routes>
     </>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,8 +6,6 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
 )
